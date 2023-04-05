bd10026bec61e949b49ebafebf20cbba84f3d4ae
Petr Krejcik <p.krejcik@stuart.com>
2023-04-06 00:31:54 +0200

    different package

--- api/edge.ts
+++ api/edge.ts
@@ -1,4 +1,4 @@
-import { initializeApp, cert, getApp } from "firebase-admin/app";
+import edgeAuth from "next-firebase-auth-edge/lib/auth";
 
 export const config = {
   runtime: "edge",
@@ -7,14 +7,11 @@ export const config = {
 // http://localhost:3000/api/edge?email=test@test.com
 
 export default async function handler(req) {
-  const type = typeof initializeApp;
-  // return initializeApp(
-  //   {
-  //     credential: cert(),
-  //     databaseURL: 'https://que-comemos-hoy-5febf.firebaseio.com'
-  //   }
-  // );
+  // const serviceAccount = JSON.parse(process.env.VITE_FIREBASE_SERVICE_ACCOUNT_KEY as string);
   const { searchParams } = new URL(req.url);
-  const email = searchParams.get("email");
-  return new Response(`type: ${type}`);
+  const token = searchParams.get("token");
+  // const auth = edgeAuth.getFirebaseAuth(serviceAccount, 'AIzaSyBfTjSCoH4xl6UFa31Eyj8h-Tf2ZxwPbmU')
+  const customToken = typeof edgeAuth.getFirebaseAuth;
+  // const customToken = auth.createCustomToken('xxx')
+  return new Response(`token: ${customToken}`);
 }
