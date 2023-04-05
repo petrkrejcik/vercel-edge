e74e995ef0addeb73d23341bab09ac551937c926
Petr Krejcik <p.krejcik@stuart.com>
2023-04-05 23:21:17 +0200

    test install firebase-admin

--- api/edge.ts
+++ api/edge.ts
@@ -1,11 +1,20 @@
+import { initializeApp, cert, getApp } from "firebase-admin/app";
+
 export const config = {
-  runtime: 'edge',
-}
+  runtime: "edge",
+};
 
 // http://localhost:3000/api/edge?email=test@test.com
 
 export default async function handler(req) {
-  const { searchParams } = new URL(req.url)
-  const email = searchParams.get('email')
-  return new Response(`Email: ${email}`)
+  const type = typeof initializeApp;
+  // return initializeApp(
+  //   {
+  //     credential: cert(),
+  //     databaseURL: 'https://que-comemos-hoy-5febf.firebaseio.com'
+  //   }
+  // );
+  const { searchParams } = new URL(req.url);
+  const email = searchParams.get("email");
+  return new Response(`type: ${type}`);
 }
