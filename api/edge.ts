f5c0fe67ca1f0d82fe193784a363cfecb731c42a
Petr Krejcik <p.krejcik@stuart.com>
2023-04-12 23:07:49 +0200

    other lib

--- api/edge.ts
+++ api/edge.ts
@@ -1,4 +1,5 @@
-import edgeAuth from "next-firebase-auth-edge/lib/auth";
+// import edgeAuth from "next-firebase-auth-edge/lib/auth";
+import { getTokenFromGCPServiceAccount } from '@sagi.io/workers-jwt';
 
 export const config = {
   runtime: "edge",
@@ -8,10 +9,11 @@ export const config = {
 
 export default async function handler(req) {
   // const serviceAccount = JSON.parse(process.env.VITE_FIREBASE_SERVICE_ACCOUNT_KEY as string);
+  
   const { searchParams } = new URL(req.url);
   const token = searchParams.get("token");
   // const auth = edgeAuth.getFirebaseAuth(serviceAccount, 'AIzaSyBfTjSCoH4xl6UFa31Eyj8h-Tf2ZxwPbmU')
-  const customToken = typeof edgeAuth.getFirebaseAuth;
+  const customToken = typeof getTokenFromGCPServiceAccount;
   // const customToken = auth.createCustomToken('xxx')
   return new Response(`token: ${customToken}`);
 }
