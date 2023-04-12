683173b66e88bd2ff502a0ae8e34bf4f3887944d
Petr Krejcik <p.krejcik@stuart.com>
2023-04-12 23:47:14 +0200

    log it

--- api/edge.ts
+++ api/edge.ts
@@ -8,12 +8,17 @@ export const config = {
 // http://localhost:3000/api/edge?email=test@test.com
 
 export default async function handler(req) {
-  // const serviceAccount = JSON.parse(process.env.VITE_FIREBASE_SERVICE_ACCOUNT_KEY as string);
-  
-  const { searchParams } = new URL(req.url);
-  const token = searchParams.get("token");
+  const serviceAccountJSON = JSON.parse(process.env.VITE_FIREBASE_SERVICE_ACCOUNT_KEY as string);
+  const aud = `https://firestore.googleapis.com/google.firestore.v1.Firestore`
+
+  const token = await getTokenFromGCPServiceAccount({ serviceAccountJSON, aud} )
+
+  console.log('token', token)
+
+  // const { searchParams } = new URL(req.url);
+  // const token = searchParams.get("token");
   // const auth = edgeAuth.getFirebaseAuth(serviceAccount, 'AIzaSyBfTjSCoH4xl6UFa31Eyj8h-Tf2ZxwPbmU')
-  const customToken = typeof getTokenFromGCPServiceAccount;
+  // const customToken = typeof getTokenFromGCPServiceAccount;
   // const customToken = auth.createCustomToken('xxx')
-  return new Response(`token: ${customToken}`);
+  return new Response(`Done`);
 }
